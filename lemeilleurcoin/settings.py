b38ee9243f27cc7552efbb27e9a652a857644cb4
Célia Prat <chatewgne@gmail.com>
2021-02-16 12:20:22 +0100

    :passport_control: users: Add login page

--- lemeilleurcoin/settings.py
+++ lemeilleurcoin/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     "lemeilleurcoin",
 ]
 
+LOGIN_REDIRECT_URL = "/"
+LOGIN_URL = "/login"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
